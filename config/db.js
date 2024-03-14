411fff43bc4d80a47fd264d41a06386c64fe97ec
ColorsKS <colorsks2021@gmail.comgit push -u origin mainremovw>
2024-03-14 15:20:54 +0530

    dbchange

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/PR_9API')
+// mongoose.connect('mongodb://localhost:27017/PR_9API')
+mongoose.connect(`mongodb+srv://Raj:uq5rMhO1FfkLkOU3@cluster0.fvudrsz.mongodb.net/demoAPI`);
 
 const db = mongoose.connection;
 
